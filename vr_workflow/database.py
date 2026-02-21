c75b76103e3994f83c629ff023d6dae79b6eda17
Tural Samadov <43365272+TuralSamadov1@users.noreply.github.com>
2026-02-21 14:49:34 +0400

    Update database.py

--- vr_workflow/database.py
+++ vr_workflow/database.py
@@ -1,1 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
 
+DATABASE_URL = "sqlite:///tasks.db"
+
+engine = create_engine(DATABASE_URL, echo=False)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base = declarative_base()
